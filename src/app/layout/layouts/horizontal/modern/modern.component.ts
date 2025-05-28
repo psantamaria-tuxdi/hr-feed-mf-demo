ceca4de661c262af99e6589fe31d018a5e8a8d8e
PabloSantaMaria <pabloj.santamaria@gmail.com>
2025-05-28 17:33:14 -0300

    add profile page and update routing to load different layouts

--- src/app/layout/layouts/horizontal/modern/modern.component.ts
+++ src/app/layout/layouts/horizontal/modern/modern.component.ts
@@ -18,7 +18,7 @@ import { NotificationsComponent } from 'app/layout/common/notifications/notifica
 import { QuickChatComponent } from 'app/layout/common/quick-chat/quick-chat.component';
 import { SearchComponent } from 'app/layout/common/search/search.component';
 import { ShortcutsComponent } from 'app/layout/common/shortcuts/shortcuts.component';
-import { UserComponent } from 'app/layout/common/user/user.component';
+// import { UserComponent } from 'app/layout/common/user/user.component';
 import { Subject, takeUntil } from 'rxjs';
 
 @Component({
@@ -37,7 +37,7 @@ import { Subject, takeUntil } from 'rxjs';
         ShortcutsComponent,
         MessagesComponent,
         NotificationsComponent,
-        UserComponent,
+        // UserComponent,
         RouterOutlet,
         QuickChatComponent,
     ],
