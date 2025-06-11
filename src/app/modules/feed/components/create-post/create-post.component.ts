3d7984a1094fb5f079f0f99a3b3a29de5969bfe4
PabloSantaMaria <pabloj.santamaria@gmail.com>
2025-06-11 16:00:59 -0300

    refactor: TG-22 update create post component to use dynamic user avatar and name

--- src/app/modules/feed/components/create-post/create-post.component.ts
+++ src/app/modules/feed/components/create-post/create-post.component.ts
@@ -1,12 +1,12 @@
 import { TextFieldModule } from '@angular/cdk/text-field';
-import { Component } from '@angular/core';
+import { Component, signal, Signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
 import { FuseCardComponent } from '@fuse/components/card';
-import { environment } from 'environments/environment';
+import { User } from '../../../../core/user/user.types';
 
 @Component({
     selector: 'hr-create-post',
@@ -25,5 +25,11 @@ import { environment } from 'environments/environment';
     },
 })
 export class CreatePostComponent {
-    baseUrl = environment.baseUrl;
+    // TODO : Replace with logged user signal
+    user: Signal<User> = signal({
+        id: '1',
+        name: 'John Doe',
+        email: 'john@doe.com',
+        avatar: 'http://localhost:4202/images/avatars/brian-hughes.jpg',
+    });
 }
