46bb53b4aab097a91233bb4f2ab6a24711ef7619
PabloSantaMaria <pabloj.santamaria@gmail.com>
2025-06-12 16:47:46 -0300

    feat: add version in both development and production environment files

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
+    version: '0.0.1-dev',
     baseUrl: 'http://localhost:4202',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
+    version: '0.0.1',
     baseUrl: '',
 };
