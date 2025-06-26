6881a8f628c54a3231d2ef221c0ef08ff81fec18
Joaquín Iglesias Coqueti <joakinic@gmail.com>
2025-06-26 13:32:08 -0300

    feat: add loading indicator to feed page with LoaderFullComponent

--- src/app/modules/feed/pages/feed/feed.page.ts
+++ src/app/modules/feed/pages/feed/feed.page.ts
@@ -3,15 +3,23 @@ import { PostComponent } from '../../components/post/post.component';
 import { CreatePostComponent } from '../../components/create-post/create-post.component';
 import { Post } from '../../../shared/types/post.types';
 import { postsMock } from '../../components/post/post.mock';
+import { LoaderFullComponent } from 'app/modules/shared/components/loader-full/loader-full.component';
 
 @Component({
     selector: 'hr-feed-page',
     templateUrl: './feed.page.html',
-    imports: [PostComponent, CreatePostComponent],
+    imports: [PostComponent, CreatePostComponent, LoaderFullComponent],
 })
 export class FeedPage {
     // TODO: Replace with real user data
     posts = signal<Post[]>(postsMock);
+    isLoading = signal<boolean>(true);
+
+    ngOnInit() {
+        setTimeout(() => {
+            this.isLoading.set(false);
+        }, 2000);
+    }
 
     /**
      * Constructor
