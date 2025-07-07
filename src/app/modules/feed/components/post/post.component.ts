97aa1a7cd2c2f1e922fc4992e5bd8155d8838e16
alesenestrari <alesenestrari@outlook.com>
2025-07-07 15:57:05 -0300

    feat(post.component): add toggleLike functionality for post likes

--- src/app/modules/feed/components/post/post.component.ts
+++ src/app/modules/feed/components/post/post.component.ts
@@ -1,14 +1,15 @@
 import { DatePipe, NgClass } from '@angular/common';
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { LikeService } from 'app/core/data/post/like.service';
+import { environment } from 'environments/environment';
+import { AvatarModule } from 'ngx-avatars';
 import { FuseCardComponent } from '../../../../../@fuse/components/card';
 import { Post } from '../../../shared/types/post.types';
 import { CommentsComponent } from '../comments/comments.component';
-import { AvatarModule } from 'ngx-avatars';
-import { environment } from 'environments/environment';
 
 @Component({
     selector: 'hr-post',
@@ -21,12 +22,35 @@ import { environment } from 'environments/environment';
         DatePipe,
         NgClass,
         CommentsComponent,
-        AvatarModule
+        AvatarModule,
     ],
     templateUrl: './post.component.html',
 })
 export class PostComponent {
+    private readonly likeService = inject(LikeService);
+
     readonly apiURL = environment.apiUrl;
 
     post = input.required<Post>();
+
+    toggleLike(): void {
+        const post = this.post();
+        const {
+            _id: postId,
+            engagement: { likes },
+        } = post;
+        const willLike = !likes.isLikedByCurrentUser;
+
+        this.likeService.toggleLike(willLike, postId).subscribe(() => {
+            this.updateLikeState(willLike, likes);
+        });
+    }
+
+    private updateLikeState(
+        isLike: boolean,
+        likes: { isLikedByCurrentUser: boolean; count: number }
+    ): void {
+        likes.isLikedByCurrentUser = isLike;
+        likes.count += isLike ? 1 : -1;
+    }
 }
