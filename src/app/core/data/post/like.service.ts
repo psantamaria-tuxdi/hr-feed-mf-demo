4c9a25483412a6b30704f8f7fec711d38e21dad0
alesenestrari <alesenestrari@outlook.com>
2025-07-07 15:56:10 -0300

    feat(like.service): implement LikeService for toggling post likes

---
+++ src/app/core/data/post/like.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Post } from 'app/modules/shared/types/post.types';
+import { environment } from 'environments/environment';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class LikeService {
+    private http = inject(HttpClient);
+    private url = (postId: string) => environment.apiUrl + `posts/${postId}/`;
+
+    toggleLike(like: boolean, postId: string) {
+        return this.http.post<Post[]>(this.url(postId) + 'toggle-like', { like });
+    }
+}
