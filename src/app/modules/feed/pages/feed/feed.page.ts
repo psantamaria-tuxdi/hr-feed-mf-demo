dd67bc6c635d5328465af097acfcd0a74c8bfa30
VnArregui <arregui.vn@gmail.com>
2025-07-07 15:51:51 -0300

    feat(post-skeleton): add posts skeleton when loading feed

--- src/app/modules/feed/pages/feed/feed.page.ts
+++ src/app/modules/feed/pages/feed/feed.page.ts
@@ -1,13 +1,13 @@
-import { Component, inject, signal } from '@angular/core';
-import { PostComponent } from '../../components/post/post.component';
+import { Component, inject } from '@angular/core';
+import { PostSkeletonComponent } from 'app/modules/shared/components/post-skeleton/post-skeleton.component';
 import { CreatePostComponent } from '../../components/create-post/create-post.component';
-import { LoaderFullComponent } from 'app/modules/shared/components/loader-full/loader-full.component';
+import { PostComponent } from '../../components/post/post.component';
 import { FeedService } from '../../services/feed.service';
 
 @Component({
     selector: 'hr-feed-page',
     templateUrl: './feed.page.html',
-    imports: [PostComponent, CreatePostComponent, LoaderFullComponent],
+    imports: [PostComponent, CreatePostComponent, PostSkeletonComponent],
 })
 export class FeedPage {
     private readonly feedService = inject(FeedService);
---
+++ src/app/modules/shared/components/post-skeleton/post-skeleton.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { FuseCardComponent } from '@fuse/components/card';
+
+@Component({
+    selector: 'hr-post-skeleton',
+    standalone: true,
+    template: `
+        <fuse-card
+            class="tw-mt-8 tw-flex tw-w-full tw-max-w-140 tw-animate-pulse tw-flex-col"
+            #expandableCard="fuseCard"
+        >
+            <!-- Header -->
+            <div
+                class="tw-mx-6 tw-mb-1 tw-mt-6 tw-flex tw-items-center sm:tw-mx-8"
+            >
+                <div
+                    class="tw-mr-4 tw-h-10 tw-w-10 tw-rounded-full tw-bg-gray-200"
+                ></div>
+                <div class="tw-flex tw-flex-col tw-gap-1">
+                    <div
+                        class="tw-h-3 tw-w-24 tw-rounded-2xl tw-bg-gray-200"
+                    ></div>
+                    <div
+                        class="tw-h-3 tw-w-16 tw-rounded-2xl tw-bg-gray-200"
+                    ></div>
+                </div>
+            </div>
+
+            <!-- Content -->
+            <div class="tw-m-6 tw-h-5 sm:tw-mx-8"></div>
+
+            <!-- Actions -->
+            <div
+                class="tw-mx-3 tw-flex tw-h-10 tw-items-center tw-text-gray-600 sm:tw-mx-5"
+            >
+                <div
+                    class="tw-ml-6 tw-mr-6 tw-h-3 tw-w-24 tw-rounded-2xl tw-bg-gray-200"
+                ></div>
+                <div
+                    class="tw-ml-6 tw-mr-6 tw-h-3 tw-w-24 tw-rounded-2xl tw-bg-gray-200"
+                ></div>
+            </div>
+
+            <hr class="tw-mx-6 tw-mb-6 tw-mt-4 tw-border-b sm:tw-mx-8" />
+
+            <div
+                class="tw-mx-6 tw-mb-4 tw-flex tw-h-10 tw-flex-col tw-justify-end sm:tw-mx-8 sm:tw-mb-6 sm:tw-flex-row sm:tw-items-center"
+            >
+                <div
+                    class="tw-ml-6 tw-mr-6 tw-h-3 tw-w-26 tw-rounded-2xl tw-bg-gray-200"
+                ></div>
+            </div>
+        </fuse-card>
+    `,
+    imports: [FuseCardComponent],
+})
+export class PostSkeletonComponent {}
