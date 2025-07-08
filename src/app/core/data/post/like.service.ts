a32090f534ec790f86416d6426fb3036f2adda54
alesenestrari <alesenestrari@outlook.com>
2025-07-08 16:14:00 -0300

    feat(post.component): enhance toggleLike method with loading state and error handling

--- src/app/core/data/post/like.service.ts
+++ src/app/core/data/post/like.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Post } from 'app/modules/shared/types/post.types';
 import { environment } from 'environments/environment';
+import { Observable } from 'rxjs';
+import { ToggleLikeResponse } from './post.types';
 
 @Injectable({
     providedIn: 'root',
@@ -10,7 +11,7 @@ export class LikeService {
     private http = inject(HttpClient);
     private url = (postId: string) => environment.apiUrl + `posts/${postId}/`;
 
-    toggleLike(postId: string) {
-        return this.http.post<Post[]>(this.url(postId) + 'toggle-like', {});
+    toggleLike(postId: string): Observable<ToggleLikeResponse> {
+        return this.http.post<ToggleLikeResponse>(this.url(postId) + 'toggle-like', {});
     }
 }
--- src/app/modules/feed/components/post/post.component.ts
+++ src/app/modules/feed/components/post/post.component.ts
@@ -11,6 +11,7 @@ import { FuseCardComponent } from '../../../../../@fuse/components/card';
 import { Post } from '../../../shared/types/post.types';
 import { CommentsComponent } from '../comments/comments.component';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { finalize } from 'rxjs';
 
 @Component({
     selector: 'hr-post',
@@ -37,6 +38,7 @@ export class PostComponent implements OnInit {
     isLikedByCurrentUser = false;
     likesCount = 0;
     topLikers = [];
+    isRequesting = false;
 
     ngOnInit(): void {
         this.isLikedByCurrentUser = this.post().engagement.likes.isLikedByCurrentUser;
@@ -45,20 +47,22 @@ export class PostComponent implements OnInit {
     }
 
     toggleLike(): void {
+        this.isRequesting = true;
         this.updateLikesState();
 
-        this.likeService.toggleLike(this.post()._id).subscribe({
-            error: () => {
-                this.snackBar.open(
-                    'Error al dar like a la publicación',
-                    'Cerrar',
-                    {
-                        duration: 3000,
+        this.likeService.toggleLike(this.post()._id)
+            .pipe(finalize(() => this.isRequesting = false))
+            .subscribe({
+                next: (response) => {
+                    if (!response.success) {
+                        this.manageError();
+                        return;
                     }
-                );
-                this.updateLikesState();
-            },
-        })
+                },
+                error: () => {
+                    this.manageError();
+                },
+            })
     }
 
     private updateLikesState() {
@@ -67,5 +71,16 @@ export class PostComponent implements OnInit {
         this.topLikers = this.isLikedByCurrentUser
             ? [...this.topLikers, this.post().author]
             : this.topLikers.filter(liker => liker._id !== this.post().author._id);
+    }
+
+    private manageError() {
+        this.snackBar.open(
+            'Error al dar like a la publicación',
+            'Cerrar',
+            {
+                duration: 3000,
+            }
+        );
+        this.updateLikesState();
     }
 }
