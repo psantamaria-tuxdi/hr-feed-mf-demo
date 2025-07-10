ef44b16ede8296ab4f757f8b3c2be11741c3392d
VnArregui <arregui.vn@gmail.com>
2025-07-10 11:45:42 -0300

    refactor(post-skeleton): pulse animation only on the elements of skeleton and darker shade of gray

--- src/app/modules/shared/components/post-skeleton/post-skeleton.component.ts
+++ src/app/modules/shared/components/post-skeleton/post-skeleton.component.ts
@@ -6,22 +6,22 @@ import { FuseCardComponent } from '@fuse/components/card';
     standalone: true,
     template: `
         <fuse-card
-            class="tw-mt-8 tw-flex tw-w-full tw-max-w-140 tw-animate-pulse tw-flex-col"
+            class="tw-mt-8 tw-flex tw-w-full tw-max-w-140 tw-flex-col"
             #expandableCard="fuseCard"
         >
             <!-- Header -->
             <div
-                class="tw-mx-6 tw-mb-1 tw-mt-6 tw-flex tw-items-center sm:tw-mx-8"
+                class="tw-mx-6 tw-mb-1 tw-mt-6 tw-flex tw-items-center sm:tw-mx-8 tw-animate-pulse"
             >
                 <div
-                    class="tw-mr-4 tw-h-10 tw-w-10 tw-rounded-full tw-bg-gray-200"
+                    class="tw-mr-4 tw-h-10 tw-w-10 tw-rounded-full tw-bg-gray-300"
                 ></div>
                 <div class="tw-flex tw-flex-col tw-gap-1">
                     <div
-                        class="tw-h-3 tw-w-24 tw-rounded-2xl tw-bg-gray-200"
+                        class="tw-h-3 tw-w-24 tw-rounded-2xl tw-bg-gray-300"
                     ></div>
                     <div
-                        class="tw-h-3 tw-w-16 tw-rounded-2xl tw-bg-gray-200"
+                        class="tw-h-3 tw-w-16 tw-rounded-2xl tw-bg-gray-300"
                     ></div>
                 </div>
             </div>
@@ -31,23 +31,23 @@ import { FuseCardComponent } from '@fuse/components/card';
 
             <!-- Actions -->
             <div
-                class="tw-mx-3 tw-flex tw-h-10 tw-items-center tw-text-gray-600 sm:tw-mx-5"
+                class="tw-mx-3 tw-flex tw-h-10 tw-items-center tw-text-gray-600 sm:tw-mx-5 tw-animate-pulse"
             >
                 <div
-                    class="tw-ml-6 tw-mr-6 tw-h-3 tw-w-24 tw-rounded-2xl tw-bg-gray-200"
+                    class="tw-ml-6 tw-mr-6 tw-h-3 tw-w-24 tw-rounded-2xl tw-bg-gray-300"
                 ></div>
                 <div
-                    class="tw-ml-6 tw-mr-6 tw-h-3 tw-w-24 tw-rounded-2xl tw-bg-gray-200"
+                    class="tw-ml-6 tw-mr-6 tw-h-3 tw-w-24 tw-rounded-2xl tw-bg-gray-300"
                 ></div>
             </div>
 
             <hr class="tw-mx-6 tw-mb-6 tw-mt-4 tw-border-b sm:tw-mx-8" />
 
             <div
-                class="tw-mx-6 tw-mb-4 tw-flex tw-h-10 tw-flex-col tw-justify-end sm:tw-mx-8 sm:tw-mb-6 sm:tw-flex-row sm:tw-items-center"
+                class="tw-mx-6 tw-mb-4 tw-flex tw-h-10 tw-flex-col tw-justify-end sm:tw-mx-8 sm:tw-mb-6 sm:tw-flex-row sm:tw-items-center tw-animate-pulse"
             >
                 <div
-                    class="tw-ml-6 tw-mr-6 tw-h-3 tw-w-26 tw-rounded-2xl tw-bg-gray-200"
+                    class="tw-ml-6 tw-mr-6 tw-h-3 tw-w-26 tw-rounded-2xl tw-bg-gray-300"
                 ></div>
             </div>
         </fuse-card>
