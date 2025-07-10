749b1d77026bc944ee53b9033fc89cac3d794524
Joaquín Iglesias Coqueti <joakinic@gmail.com>
2025-07-10 10:54:29 -0300

    Revert "feat(post.component): implement comments loading functionality and update template for comments display"
    This reverts commit 56ea70834e5e733a8e106455b02cea63c56152fe.

--- src/app/modules/feed/components/post/post.component.ts
+++ src/app/modules/feed/components/post/post.component.ts
@@ -1,23 +1,14 @@
 import { DatePipe, NgClass } from '@angular/common';
-import {
-    Component,
-    computed,
-    DestroyRef,
-    inject,
-    input,
-    signal,
-} from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Component, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
-import { CommentService } from 'app/core/data/post/comment.service';
-import { environment } from 'environments/environment';
-import { AvatarModule } from 'ngx-avatars';
 import { FuseCardComponent } from '../../../../../@fuse/components/card';
 import { Post } from '../../../shared/types/post.types';
 import { CommentsComponent } from '../comments/comments.component';
+import { AvatarModule } from 'ngx-avatars';
+import { environment } from 'environments/environment';
 
 @Component({
     selector: 'hr-post',
@@ -30,30 +21,12 @@ import { CommentsComponent } from '../comments/comments.component';
         DatePipe,
         NgClass,
         CommentsComponent,
-        AvatarModule,
+        AvatarModule
     ],
     templateUrl: './post.component.html',
 })
 export class PostComponent {
-    private readonly commentsService = inject(CommentService);
-    private _destroyRef = inject(DestroyRef);
     readonly apiURL = environment.apiUrl;
 
     post = input.required<Post>();
-    id = computed(() => this.post()._id);
-    comments = computed(() => this.post().engagement.comments);
-    isLoading = signal<boolean>(false);
-
-    loadComments() {
-        if (this.comments().items) return; // Comments already loaded
-
-        this.isLoading.set(true);
-        this.commentsService
-            .getAll(this.id())
-            .pipe(takeUntilDestroyed(this._destroyRef))
-            .subscribe((items) => {
-                this.comments().items = items;
-                this.isLoading.set(false);
-            });
-    }
 }
