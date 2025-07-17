12b3a4673d727ac5f6635b701e70a5773ab30882
Joaquín Iglesias Coqueti <joakinic@gmail.com>
2025-07-17 16:34:53 -0300

    refactor(avatar): remove unused Input import in AvatarComponent

--- src/app/modules/shared/components/avatar/avatar.component.ts
+++ src/app/modules/shared/components/avatar/avatar.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { User } from 'app/core/user/user.types';
 import { Author } from '../../types/author.types';
 import { AvatarModule } from 'ngx-avatars';
