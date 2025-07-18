0c5a9c954308531a953f38000372f2299b30a67c
Joaquín Iglesias Coqueti <joakinic@gmail.com>
2025-07-18 18:05:30 -0300

    feat(feed): implement infinite scroll and improve loading logic

--- src/app/modules/feed/pages/feed/feed.page.ts
+++ src/app/modules/feed/pages/feed/feed.page.ts
@@ -1,21 +1,27 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { PostSkeletonComponent } from 'app/modules/post/components/post-skeleton.component';
 import { CreatePostComponent } from '../../components/create-post/create-post.component';
 import { PostComponent } from '../../../post/post.component';
 import { FeedService } from '../../services/feed.service';
+import { InfiniteScrollDirective } from 'ngx-infinite-scroll';
 
 @Component({
     selector: 'hr-feed-page',
     templateUrl: './feed.page.html',
-    imports: [PostComponent, CreatePostComponent, PostSkeletonComponent],
+    imports: [PostComponent, CreatePostComponent, PostSkeletonComponent, InfiniteScrollDirective],
 })
-export class FeedPage {
+export class FeedPage implements OnInit {
     private readonly feedService = inject(FeedService);
     posts = this.feedService.feed;
     isLoading = this.feedService.isLoading;
+    hasMore = this.feedService.hasMore;
 
     ngOnInit() {
         this.feedService.load();
+    }
+
+    fetchMore() {
+        this.feedService.fetchMore();
     }
 
     /**
