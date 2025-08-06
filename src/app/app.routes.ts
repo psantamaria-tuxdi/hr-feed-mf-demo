61d5e7f70f09751d655707c85e280a4b2b3eb0e7
Joaquín Iglesias Coqueti <joakinic@gmail.com>
2025-08-06 18:47:12 -0300

    enable multiple HR-social-media base routes for humanage environment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,39 +2,35 @@ import { Route } from '@angular/router';
 import { AuthGuard } from 'app/core/auth/guards/auth.guard';
 // import { NoAuthGuard } from 'app/core/auth/guards/noAuth.guard';
 import { LayoutComponent } from 'app/layout/layout.component';
+import { environment } from 'environments/environment';
+
+const hrSocialMediaPaths =
+  environment.name === 'humanage'
+    ? // HUMANAGE:
+      ['employee/HR-social-media', 'employer/HR-social-media']
+    : // AXTON:
+      ['employer/HR-social-media'];
 
 // @formatter:off
 /* eslint-disable max-len */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 export const appRoutes: Route[] = [
   // Redirect empty path to '/example'
-  { path: '', pathMatch: 'full', redirectTo: 'employer/HR-social-media' },
-
-  // {
-  //     path: 'employer/HR-social-media',
-  //     loadChildren: () => import('app/modules/profile/profile.routes'),
-  // },
-
-  {
-    path: 'employer/HR-social-media',
+  //   { path: '', pathMatch: 'full', redirectTo: 'employer/HR-social-media' },
+  ...hrSocialMediaPaths.map((path) => ({
+    path,
     component: LayoutComponent,
     data: {
       layout: 'empty',
     },
-    // canActivateChild: [AuthGuard],
     children: [
       {
         canActivate: [AuthGuard],
         path: '',
         loadChildren: () => import('app/modules/feed/feed.routes'),
-        pathMatch: 'full',
-      },
-      {
-        path: 'profile',
-        loadChildren: () => import('app/modules/profile/profile.routes'),
       },
     ],
-  },
+  })),
 
   // {
   //     path: '',
