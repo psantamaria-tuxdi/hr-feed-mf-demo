72675451e7bc728f1686ccd46bd3034ebaab7d1e
Pablo Santa María <psantamaria@tuxdi.com>
2025-08-28 15:00:56 -0300

    fix: update baseUrl to point to the correct production endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     version: '0.0.1',
-    baseUrl: 'https://68b0608f1e57354397e1f15c--fluffy-genie-01c27a.netlify.app',
+    baseUrl: 'https://hr-feed-host.netlify.app',
     // apiUrl: 'http://localhost:3000/api/',
     apiUrl: 'https://1s6r6nll-3000.brs.devtunnels.ms/api/',
     name: 'axton',
