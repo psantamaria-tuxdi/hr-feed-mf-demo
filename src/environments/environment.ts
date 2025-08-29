7b9139188ed9de69e1d4d70028e99c5d2a0a6cb2
Pablo Santa María <psantamaria@tuxdi.com>
2025-08-29 15:31:25 -0300

    fix: update apiUrl to point to the correct API endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
     version: '0.0.1',
     baseUrl: 'https://hr-feed-host.netlify.app',
     // apiUrl: 'http://localhost:3000/api/',
-    apiUrl: 'https://1s6r6nll-3000.brs.devtunnels.ms/api/',
+    apiUrl: 'https://hr-feed-api.jacket.tux.ar/api/',
     name: 'axton',
     // name: 'humanage',
     // host: 'https://68b0608f1e57354397e1f15c--fluffy-genie-01c27a.netlify.app/'
